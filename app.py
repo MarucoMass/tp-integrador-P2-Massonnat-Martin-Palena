fe6fc6dfc8f26e0e3dc4264618f8c858db986780
Maximo Martin <marmax0504@gmail.com>
2024-06-14 18:50:11 -0300

    creacion de app y datos

--- app.py
+++ app.py
@@ -18,3 +18,75 @@
 #     else:
 #         print("No tienes objetos")
 #         break
+# return f'''Nro: {self.nro_viaje} - {self.nombre}
+#         Fecha Desde: {self.fecha_dede}
+#         Fecha hasta: {self.fecha_hasta}
+#         Días: [{self.cantidad_dias}]
+#         Costo: $ {self.costo}'''
+
+def capturarPokemon():
+    pass
+
+def retarLiderGimnasio():
+    pass
+
+def aplicarObjeto():
+    pass
+
+def comprarObjeto():
+    pass
+
+def verPokedex():
+    pass
+
+
+
+
+
+
+
+
+def menu():
+    print(f'''1- Explorar
+    2- Aplicar objeto a tu pokemon
+    3- Comprar Objeto
+    4- Ver tu pokedex
+    5- Salir
+          ''')
+
+def subMenu():
+    print(f''' 1- Salir a capturar pokemons
+          2- Retar a lider de gimnasio
+          
+          ''')
+
+while True:
+    
+    menu()
+    
+    opt = int(input("Ingrese una opcion: "))
+    if opt == 1:
+        subMenu()
+        opt1 = int(input("Ingrese una opcion: "))
+        if opt1 == 1:
+            capturarPokemon()
+            pass
+        elif opt1 == 2:
+            retarLiderGimnasio()
+            pass
+        else:
+            print("Opcion no valida")
+    elif opt == 2:
+        aplicarObjeto()
+        pass
+    elif opt == 3:
+        comprarObjeto()
+        pass
+    elif opt == 4:
+        verPokedex()
+        pass
+    elif opt == 5:
+        print("Chau aguante digimon")
+        break
+    else:
+        print("Opcion incorrecta")
---
+++ datos.py
@@ -0,0 +1,13 @@
+from entrenador import *
+from pokedex import *
+from pokemon import *
+from gimnasio import *
+from medalla import *
+from habilidad import *
+from objeto import *
+from probabilidad_captura import calcular_probabilidad_captura
+
+
+
+
+lista_entrenadores = [Entrenador(), Entrenador(), Entrenador(), Entrenador()]
