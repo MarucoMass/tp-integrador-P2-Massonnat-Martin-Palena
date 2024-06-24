ea3730fb71d4ef9fee12e3dd85629e36b6baf5b4
Maximo Martin <marmax0504@gmail.com>
2024-06-24 16:00:00 -0300

    agrego entrenadores y gimnasios en la parte datos

--- datos.py
+++ datos.py
@@ -10,29 +10,29 @@ from calcular_probabilidad import calcular_probabilidad
 
 
 
-lista_pokemons = [Pokemon("Pikachu", 25, "Eléctrico", 100, 40, "Impactrueno", 57),
-    Pokemon("Charizard", 36, "Fuego", 150, 78, "Impactrueno", 88),
-    Pokemon("Bulbasaur", 16, "Planta", 120, 49, "Impactrueno", 76),
-    Pokemon("Squirtle", 10, "Agua", 90, 48, "Impactrueno", 23),
-    Pokemon("Jigglypuff", 12, "Normal", 110, 20, "Impactrueno", 89),
-    Pokemon("Gengar", 28, "Fantasma", 120, 60, "Impactrueno", 12),
-    Pokemon("Eevee", 18, "Normal", 80, 50, "Impactrueno", 73),
-    Pokemon("Snorlax", 50, "Normal", 300, 65, "Impactrueno", 22),
-    Pokemon("Dragonite", 55, "Dragón", 200, 95, "Impactrueno", 87),
-    Pokemon("Mewtwo", 70, "Psíquico", 200,  90, "Impactrueno", 56),
-    Pokemon("Gyarados", 35, "Agua", 150, 79, "Impactrueno", 51),
-    Pokemon("Arcanine", 40, "Fuego", 200,  80, "Impactrueno", 52),
-    Pokemon("Machamp", 32, "Lucha", 160,  75, "Impactrueno", 56),
-    Pokemon("Lapras", 45, "Agua/Hielo", 85, 80, "Impactrueno", 75),
-    Pokemon("Alakazam", 38, "Psíquico", 105, 55, "Impactrueno", 91),
-    Pokemon("Golem", 42, "Roca", 180,  130, "Impactrueno", 11),
-    Pokemon("Blastoise", 36, "Agua", 170, 100, "Impactrueno", 54),
-    Pokemon("Venusaur", 36, "Planta", 180, 83, "Impactrueno", 58),
-    Pokemon("Jolteon", 30, "Eléctrico", 65, 60, "Impactrueno", 72),
-    Pokemon("Flareon", 30, "Fuego", 130, 60, "Impactrueno", 82)]
+lista_pokemons = [ Pokemon("Pikachu", 25, "Eléctrico", 100, 50, "Impactrueno", 40),
+    Pokemon("Charmander", 15, "Fuego", 80, 40, "Llamarada", 60),
+    Pokemon("Bulbasaur", 20, "Planta", 90, 45, "Rayo Solar", 55),
+    Pokemon("Squirtle", 18, "Agua", 85, 48, "Hidrobomba", 70),
+    Pokemon("Jigglypuff", 22, "Normal", 120, 35, "Canto", 25),
+    Pokemon("Meowth", 17, "Normal", 75, 40, "Golpe Furia", 45),
+    Pokemon("Psyduck", 23, "Agua", 95, 50, "Confusión", 50),
+    Pokemon("Machop", 19, "Lucha", 100, 55, "Golpe Karate", 60),
+    Pokemon("Geodude", 21, "Roca", 110, 60, "Avalancha", 65),
+    Pokemon("Gastly", 24, "Fantasma", 70, 30, "Bola Sombra", 80),
+    Pokemon("Eevee", 16, "Normal", 85, 45, "Rapidez", 50),
+    Pokemon("Snorlax", 30, "Normal", 160, 70, "Hiperrayo", 100),
+    Pokemon("Gengar", 28, "Fantasma", 85, 50, "Puño Sombra", 85),
+    Pokemon("Onix", 26, "Roca", 120, 70, "Terremoto", 75),
+    Pokemon("Arcanine", 29, "Fuego", 105, 55, "Llamarada", 90),
+    Pokemon("Gyarados", 32, "Agua", 115, 60, "Hidrobomba", 100),
+    Pokemon("Lapras", 27, "Agua", 130, 65, "Surf", 85),
+    Pokemon("Ditto", 20, "Normal", 70, 40, "Transformación", 0),
+    Pokemon("Vulpix", 15, "Fuego", 75, 38, "Lanzallamas", 55),
+    Pokemon("Alakazam", 35, "Psíquico", 80, 45, "Psíquico", 95)]
 
 
-lista_pokedex = [Pokedex(), Pokedex(), Pokedex()]
+lista_pokedex = [Pokedex(), Pokedex(), Pokedex(), Pokedex,  Pokedex(), Pokedex()]
 lista_pokedex[1].agregar_pokemon(lista_pokemons[5])
 lista_pokedex[1].agregar_pokemon(lista_pokemons[2])
 lista_pokedex[1].agregar_pokemon(lista_pokemons[3])
@@ -44,12 +44,18 @@ lista_pokedex[1].agregar_pokemon(lista_pokemons[7])
 lista_entrenadores = [
     Entrenador("Maximus", lista_pokedex[0]), 
     Entrenador("Aurelio", lista_pokedex[1]), 
-    Entrenador("Blackets", lista_pokedex[2])
+    Entrenador("Blackets", lista_pokedex[2]),
+    Entrenador("Zephyr", lista_pokedex[3]),
+    Entrenador("Pyro", lista_pokedex[4]),
+    Entrenador("Aquarius", lista_pokedex[5])
     ]
 
 lista_medallas = [
     Medalla("Medalla Roca"), 
-    Medalla("Medalla Rayo")
+    Medalla("Medalla Rayo"),
+    Medalla("Medalla Viento"),
+    Medalla("Medalla Fuego"),
+    Medalla("Medalla Agua")
     ]
 
 lista_objetos = [
@@ -70,6 +76,12 @@ lista_entrenadores[1].agregar_pokemon(lista_pokemons[17])
 lista_entrenadores[2].agregar_pokemon(lista_pokemons[18])
 lista_entrenadores[2].agregar_pokemon(lista_pokemons[19])
 
+lista_entrenadores[3].agregar_pokemon(lista_pokemons[7])  
+
+lista_entrenadores[4].agregar_pokemon(lista_pokemons[14]) 
+
+lista_entrenadores[5].agregar_pokemon(lista_pokemons[16]) 
+
 #lista_entrenadores[0].elegirPokemon(0)
 lista_entrenadores[1].elegir_pokemon(0)
 lista_entrenadores[2].elegir_pokemon(0)
@@ -86,7 +98,10 @@ lista_entrenadores[2].elegir_pokemon(0)
 
 lista_gimnasios = [
                 Gimnasio("La Roca", lista_entrenadores[1], lista_medallas[0]),
-                Gimnasio("El Rayo", lista_entrenadores[2], lista_medallas[1])
+                Gimnasio("El Rayo", lista_entrenadores[2], lista_medallas[1]),
+                Gimnasio("El Viento", lista_entrenadores[3], lista_medallas[2]),
+                Gimnasio("El Fuego", lista_entrenadores[4], lista_medallas[3]),
+                Gimnasio("El Agua", lista_entrenadores[5], lista_medallas[4])
                 ]
 
 
